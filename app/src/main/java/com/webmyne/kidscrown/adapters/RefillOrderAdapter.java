eb60cbeac83bb58598170c627ca49d0374f763cf
dhruvil <dd.droid@yahoo.com>
2015-08-18 15:28:45 +0530

    tguy

---
+++ app/src/main/java/com/webmyne/kidscrown/adapters/RefillOrderAdapter.java
@@ -0,0 +1,87 @@
+package com.webmyne.kidscrown.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.webmyne.kidscrown.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dhruvil on 17-08-2015.
+ */
+public class RefillOrderAdapter extends BaseAdapter {
+
+    private Context _ctx;
+    private ArrayList<String> crowns;
+    private OnDeleteListner onDeleteListner;
+
+    public RefillOrderAdapter(Context _ctx, ArrayList<String> crowns) {
+        this._ctx = _ctx;
+        this.crowns = crowns;
+    }
+
+    @Override
+    public int getCount() {
+        return crowns.size();
+    }
+
+    class ViewHolder {
+        TextView txtName;
+        EditText edQty;
+        ImageView imgDelete;
+    }
+
+    public void setOnDeleteListner(OnDeleteListner listner) {
+        this.onDeleteListner = listner;
+
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        LayoutInflater mInflater = (LayoutInflater) _ctx.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.item_cart, parent, false);
+            holder = new ViewHolder();
+            holder.txtName = (TextView) convertView.findViewById(R.id.txtItemName);
+            holder.imgDelete = (ImageView) convertView.findViewById(R.id.imgDeleteCart);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.txtName.setText(crowns.get(position));
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDeleteListner.onDelete(position);
+            }
+        });
+
+        return convertView;
+    }
+
+    public interface OnDeleteListner {
+        public void onDelete(int position);
+    }
+}
--- app/src/main/java/com/webmyne/kidscrown/ui/RefillActivity.java
+++ app/src/main/java/com/webmyne/kidscrown/ui/RefillActivity.java
@@ -1,6 +1,8 @@
 package com.webmyne.kidscrown.ui;
 
 import android.graphics.Color;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,41 +10,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.webmyne.kidscrown.R;
+import com.webmyne.kidscrown.adapters.RefillOrderAdapter;
 import com.webmyne.kidscrown.helper.Functions;
 import com.webmyne.kidscrown.ui.widgets.CrownQuadrant;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RefillActivity extends AppCompatActivity {
+public class RefillActivity extends AppCompatActivity implements CrownQuadrant.OnCrownClickListner,RefillOrderAdapter.OnDeleteListner{
 
-   private android.support.v7.widget.Toolbar toolbar;
+    private android.support.v7.widget.Toolbar toolbar;
     LinearLayout crownSetLayout;
-
     private CrownQuadrant upperLeft;
     private CrownQuadrant upperRight;
     private CrownQuadrant lowerLeft;
     private CrownQuadrant lowerRight;
-
-
+    // private ImageView imgAdd;
+    private ArrayList<String> orderArray;
+    private ListView listRefill;
+    RefillOrderAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refill);
+
         init();
-
-
-
-
 
     }
 
     private void init() {
-       toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         crownSetLayout = (LinearLayout) findViewById(R.id.crownSetLayout);
         if (toolbar != null) {
             toolbar.setNavigationIcon(R.drawable.ic_action_navigation_arrow_back);
@@ -62,39 +67,47 @@ public class RefillActivity extends AppCompatActivity {
         lowerLeft = (CrownQuadrant) findViewById(R.id.quadLowerLeft);
         lowerRight = (CrownQuadrant) findViewById(R.id.quadLowerRight);
 
+        upperLeft.setOnCrownClickListner(this);
+        upperRight.setOnCrownClickListner(this);
+        lowerLeft.setOnCrownClickListner(this);
+        lowerRight.setOnCrownClickListner(this);
+
         upperLeft.setUpperLeft();
         upperRight.setUpperRight();
         lowerLeft.setLowerLeft();
         lowerRight.setLowerRight();
 
+        listRefill = (ListView)findViewById(R.id.listRefill);
+        orderArray = new ArrayList<>();
+        adapter = new RefillOrderAdapter(this,orderArray);
+        adapter.setOnDeleteListner(this);
+        listRefill.setAdapter(adapter);
 
-        Button btnAdd = (Button)findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                HashMap<String,String> orderMapUL = upperLeft.getValues();
-                HashMap<String,String> orderMapUR = upperRight.getValues();
-                HashMap<String,String> orderMapLL = lowerLeft.getValues();
-                HashMap<String,String> orderMapLR = lowerRight.getValues();
-                HashMap<String,String> orderMap = new HashMap<String, String>();
-
-                orderMap.putAll(orderMapUL);
-                orderMap.putAll(orderMapUR);
-                orderMap.putAll(orderMapLL);
-                orderMap.putAll(orderMapLR);
-
-                for(Map.Entry<String,String> map : orderMap.entrySet()){
-                    Log.e("Item",String.format("%s * %s",map.getKey(),map.getValue()));
-                }
+    }
 
 
+    @Override
+    public void add(String value) {
+        orderArray.add(value);
+        adapter.notifyDataSetChanged();
+    }
 
-            }
-        });
+    @Override
+    public void delete(String value) {
+        orderArray.remove(value);
+        adapter.notifyDataSetChanged();
+    }
 
+    @Override
+    public void onDelete(int position) {
 
+        String toDelete = orderArray.get(position);
+        upperLeft.removeSelected(toDelete);
+        upperRight.removeSelected(toDelete);
+        lowerLeft.removeSelected(toDelete);
+        lowerRight.removeSelected(toDelete);
 
-
+        orderArray.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
