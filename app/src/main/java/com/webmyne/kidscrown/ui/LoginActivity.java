f26fca0f6115f14908033805d899c6518f96fafa
priyasindkar <sindkar.priya@gmail.com>
2015-08-25 18:49:01 +0530

    as

--- app/src/main/java/com/webmyne/kidscrown/ui/LoginActivity.java
+++ app/src/main/java/com/webmyne/kidscrown/ui/LoginActivity.java
@@ -2,16 +2,40 @@ package com.webmyne.kidscrown.ui;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import com.google.gson.GsonBuilder;
 import com.webmyne.kidscrown.R;
 import com.webmyne.kidscrown.helper.CallWebService;
@@ -21,20 +45,53 @@ import com.webmyne.kidscrown.helper.Functions;
 import com.webmyne.kidscrown.model.UserProfile;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginActivity extends ActionBarActivity {
+import java.util.Arrays;
 
-    Button btnLogin, btnFb, btnGplus;
+public class LoginActivity extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    Button btnLogin;
+    SignInButton btnGplus;
     TextView txtRegister, txtForgot;
     EditText edtUsername, edtPassword;
     String name, password, url;
     ProgressDialog pd;
 
+    private static final int RC_SIGN_IN = 0;
+    // Logcat tag
+    private static final String TAG = "MainActivity";
+
+    // Profile pic image size in pixels
+    private static final int PROFILE_PIC_SIZE = 400;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * A flag indicating that a PendingIntent is in progress and prevents us
+     * from starting further intents.
+     */
+    private boolean mIntentInProgress;
+
+    private boolean mSignInClicked;
+
+    private ConnectionResult mConnectionResult;
+
+    // private LoginButton btnFb;
+    private LinearLayout linearFbLogin;
+    private CallbackManager callbackManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
 
         init();
 
@@ -55,6 +112,78 @@ public class LoginActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Functions.fireIntent(LoginActivity.this, RegisterActivity.class);
+            }
+        });
+
+        btnGplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < btnGplus.getChildCount(); i++) {
+                    View view = btnGplus.getChildAt(i);
+
+                    if (view instanceof TextView) {
+                        if(((TextView) view).getText().toString().contains("SignIn")) {
+                            signInWithGplus();
+                        } else {
+                            signOutFromGplus();
+                        }
+                        break;
+                    }
+                }
+
+            }
+    });
+
+
+        linearFbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile, email, user_birthday, user_friends"));
+            }
+        });
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.e("USER_ID", loginResult.getAccessToken().getUserId());
+
+                // App code
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.e("LoginActivity", response.toString());
+                                JSONObject profile = response.getJSONObject();
+                                try {
+                                    Log.e("User_Name", profile.get("name").toString());
+                                    Log.e("User_Email", profile.get("email").toString());
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Log.e("reponse", response.getJSONObject().toString());
+
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
             }
         });
     }
@@ -108,8 +237,178 @@ public class LoginActivity extends ActionBarActivity {
         txtRegister = (TextView) findViewById(R.id.txtRegister);
         edtUsername = (EditText) findViewById(R.id.edtUsername);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
-        btnFb = (Button) findViewById(R.id.btnFb);
-        btnGplus = (Button) findViewById(R.id.btnGplus);
+        //btnFb = (LoginButton) findViewById(R.id.btnFb);
+        linearFbLogin = (LinearLayout) findViewById(R.id.linearFbLogin);
+        btnGplus = (SignInButton) findViewById(R.id.btnGplus);
+        setGooglePlusButtonText(btnGplus, "SignIn With Google Plus");
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Method to resolve any signin errors
+     * */
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+                    0).show();
+            return;
+        }
+
+        if (!mIntentInProgress) {
+            // Store the ConnectionResult for later usage
+            mConnectionResult = result;
+
+            if (mSignInClicked) {
+                // The user has already clicked 'sign-in' so we attempt to
+                // resolve all
+                // errors until the user is signed in, or they cancel.
+                resolveSignInError();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode,
+                                    Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, responseCode, intent);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        mSignInClicked = false;
+        //Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+        View view = findViewById(android.R.id.content);
+        Functions.snack(view, "User is connected via Google Plus!");
+        // Get user's information
+        getGPlusProfileInformation();
+        setGooglePlusButtonText(btnGplus, "Logout From Google Plus");
+        // Update the UI after signin
+        //updateUI(true);
+
+    }
+
+    /**
+     * Fetching user's information name, email, profile pic
+     * */
+    private void getGPlusProfileInformation() {
+        try {
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person currentPerson = Plus.PeopleApi
+                        .getCurrentPerson(mGoogleApiClient);
+                String personName = currentPerson.getDisplayName();
+                String personPhotoUrl = currentPerson.getImage().getUrl();
+                String personGooglePlusProfile = currentPerson.getUrl();
+                String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+                Log.e("GPLUS USER_NAME", personName);
+                Log.e("GPLUS USER_Email", email);
+                Log.e("GPLUS USER_PROFILE", personGooglePlusProfile);
+
+
+                // by default the profile url gives 50x50 px image only
+                // we can replace the value with whatever dimension we want by
+                // replacing sz=X
+                personPhotoUrl = personPhotoUrl.substring(0,
+                        personPhotoUrl.length() - 2)
+                        + PROFILE_PIC_SIZE;
+
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Person information is null", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+        //updateUI(false);
+    }
+
+    /**
+     * Sign-in into google
+     * */
+    private void signInWithGplus() {
+        if (!mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            resolveSignInError();
+        }
+    }
+
+    /**
+     * Sign-out from google
+     * */
+    private void signOutFromGplus() {
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+            mGoogleApiClient.connect();
+            setGooglePlusButtonText(btnGplus, "SignIn With GooglePlus");
+            //updateUI(false);
+        }
+    }
+
+
+    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setTextSize(15);
+                tv.setTypeface(null, Typeface.BOLD);
+                tv.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+                tv.setText(buttonText);
+                tv.setPadding(0,0,0,0);
+                return;
+            }
+        }
     }
 
 }
